c56a331ff64a1e81bc7901c1dda4007b38c27eb1
sergeygrif <149859984+sergeygrif@users.noreply.github.com>
2026-04-19 11:49:07 +0300

    Update Дайсчесс.cpp

--- Дайсчесс.cpp
+++ Дайсчесс.cpp
@@ -10455,13 +10455,10 @@ if(p==-1||p==-8421505||p==-16777216)return 1;
 return 0;
 }
 vector<int> DICEVECTOR(vector<int>& s){
-int i,j,k,c;
+int i,j,k;
 vector<int> dice;
 dice={0,0,0};
-for(i=0;i<3;i++)for(j=0;j<3;j++)for(k=0;k<3;k++){
-c=s[690+227*i+42*j+3840*(585+45*k)];
-dice[i]+=c==-1||c==-394759||c==-8421505||c==-8487298||c==-16777216;
-}
+for(i=0;i<3;i++)for(j=0;j<3;j++)for(k=0;k<3;k++)dice[i]+=DICE(690+227*i+42*j,585+45*k,s);
 return dice;
 }
 int DICERAW(vector<int> dice){
