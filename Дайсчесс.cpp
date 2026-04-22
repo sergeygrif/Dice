87e8d958eacb6272bfc3d35298da5163da0fb04d
sergeygrif <149859984+sergeygrif@users.noreply.github.com>
2026-04-22 12:08:09 +0300

    Update Дайсчесс.cpp

--- Дайсчесс.cpp
+++ Дайсчесс.cpp
@@ -10483,9 +10483,12 @@ for(i=0;i<5;i++)while(dicePiece[dice][i]&&(pos.color[pos.side]&pos.piece[i])==0&
 return dice;
 }
 int EQUAL(vector<int>& s1,vector<int>& s2){
-int i,j,k;
-if(s1.empty())return 0;
-for(i=0;i<3;i++)for(j=0;j<158;j++)for(k=0;k<158;k++)if(s2[655+227*i+j+1920*(550+k)]!=s1[655+227*i+j+1920*(550+k)])return 0;
+int i,j,k,n;
+if(s2.size()!=s1.size()||STATE(s2)!=STATE(s1))return 0;
+for(i=0;i<3;i++)for(j=0;j<158;j++)for(k=0;k<158;k++){
+n=655+227*i+j+1920*(550+k);
+if(s2[n]!=s1[n])return 0;
+}
 return 1;
 }
 void SET(vector<int>& s,Position& pos,array<uint64_t,4>& path,array<int,64>& mask){
