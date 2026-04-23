c3b54d37f1d8f36de407552d10dfad1d33961d29
sergeygrif <149859984+sergeygrif@users.noreply.github.com>
2026-04-23 15:23:12 +0300

    Update Дайсчесс.cpp

--- Дайсчесс.cpp
+++ Дайсчесс.cpp
@@ -10560,6 +10560,51 @@ POS.key==computeKey(POS);
 s1=s2;
 board1=board2;
 }
+void LOAD(){
+int side,from,to,dice;
+array<int,64> board1,board2;
+vector<int> s1,s2,square;
+while(1){
+s2=NEW(s1);
+if(STATE(s2)==-1){
+START();
+END(board1,board2,s1,s2);
+continue;
+}
+board2=BOARD(s2);
+BOARD(board2);
+POS.side=SIDE(s2);
+if(POS.dice==0){
+POS.ep1[!POS.side]=0;
+POS.ep2=0;
+DICE(s2);
+END(board1,board2,s1,s2);
+continue;
+}
+side=SIDE(s1);
+square=SQUARE(board1,board2);
+if(square.size()==4){
+POS.castle&=12-9*side;
+POS.dice=newDice[POS.dice][5];
+POS.dice=newDice[POS.dice][3];
+END(board1,board2,s1,s2);
+continue;
+}
+if(square.size()==3){
+POS.dice=newDice[POS.dice][0];
+END(board1,board2,s1,s2);
+continue;
+}
+from=square[0];
+to=square[1];
+dice=board1[from]%6;
+if(dice==0&&(from^to)==16)POS.ep1[!side]|=bit((from+to)/2);
+if(dice==0&&POS.ep1[side]&epMask[to])POS.ep2|=bit(to);
+POS.castle&=~(MASK[from]|MASK[to]);
+POS.dice=newDice[POS.dice][dice];
+END(board1,board2,s1,s2);
+}
+}
 void SET(vector<int>& s,Position& pos,array<uint64_t,4>& path,array<int,64>& mask){
 int sq,piece;
 array<int,64> board;
