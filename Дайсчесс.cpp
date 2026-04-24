f20c64bf2fb98de93d31dd8c9ba46ea0ab4b9fe5
sergeygrif <149859984+sergeygrif@users.noreply.github.com>
2026-04-24 23:55:41 +0300

    Update Дайсчесс.cpp

--- Дайсчесс.cpp
+++ Дайсчесс.cpp
@@ -10438,7 +10438,11 @@ DeleteObject(d);
 return s;
 }
 vector<int> S(){return S(0,1919,0,2399);}
-int DIFF(int a,int b){return abs(b&255-a&255)+abs((b>>8)&255-(a>>8)&255)+abs((b>>16)&255-(a>>16)&255);}
+int DIFF(int a,int b){
+a+=16777216;
+b+=16777216;
+return abs(b%256-a%256)+abs(b/256%256-a/256%256)+abs(b/65536-a/65536);
+}
 int STATE(vector<int>& s){
 if(s.empty()||s[960+1920*295]!=-469625||s[960+1920*630]==-15324372||s[1335+1920*980]==-15189205)return -1;
 return s[1442+1920*1935]==-15785961;
